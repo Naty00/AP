60d9f885fea300dc281ec25b36f0203b7b9d7afa
Naty00 <naty_ch2000@hotmail.com>
2017-07-10 18:46:51 -0300

    Add files via upload

---
+++ Rompecabezas/Recursos/js/juego.js
@@ -0,0 +1,153 @@
+// Representación de la grilla. Cada nro representa a una pieza.
+// El 9 es la posición vacía
+var grilla = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+// Ac&aacute; vamos a ir guardando la posición vacía
+var posicionVacia = {
+  fila:2,
+  columna:2
+};
+// Esta función va a chequear si el Rompecabezas est&aacute; en la posición ganadora
+function chequearSiGano(){
+  var x =1;
+  for(var i=0;i<3;i++)
+  {
+    for(var j=0;j<3;j++)
+    {
+      if(grilla[i][j]!=x)
+      {
+        return false;
+      }
+      x++;
+    }
+  }
+  return true;
+}
+
+
+// la hacen los alumnos, pueden mostrar el cartel como prefieran. Pero es importante que usen
+// esta función
+function mostrarCartelGanador()
+{ 
+  alert("FELICITACIONES");
+
+}
+ 
+// Intercambia posiciones grilla y en el DOM
+function intercambiarPosiciones(fila1, columna1, fila2, columna2)
+{ 
+  columna2 = parseInt(columna2);
+  console.log("llegue hasta acaá");
+  console.log(fila1, columna1);
+  console.log(fila2, columna2);
+  var pieza1=grilla[fila1][columna1];
+  var pieza2=grilla[fila2][columna2];
+     grilla[fila1][columna1]=pieza2;  
+     grilla[fila2][columna2]=pieza1;
+
+     var elementoPieza1=document.getElementById("pieza"+pieza1);
+     var elementoPieza2=document.getElementById("pieza"+pieza2);
+     var padre=document.getElementById("juego"); 
+
+    var clonElemento1= elementoPieza1.cloneNode(true);
+    var clonElemento2= elementoPieza2.cloneNode(true);
+
+               padre.replaceChild(clonElemento1, elementoPieza2);
+               padre.replaceChild(clonElemento2, elementoPieza1);
+//DOM
+}  
+// Actualiza la posición de la pieza vacía
+function actualizarPosicionVacia(nuevaFila,nuevaColumna){
+posicionVacia.fila=nuevaFila;
+posicionVacia.columna=nuevaColumna;
+}
+
+
+// Para chequear si la posicón está dentro de la grilla.
+function posicionValida(fila, columna){
+  if(fila>2||fila<0||columna>2||columna<0)
+    return false;
+  return true;
+}
+
+// Movimiento de fichas, en este caso la que se mueve es la blanca intercambiando
+// su posición con otro elemento
+function moverEnDireccion(direccion){
+  console.log("muevo en " +direccion);
+
+  var nuevaFilaPiezaVacia;
+  var nuevaColumnaPiezaVacia;
+
+  // Intercambia pieza blanca con la pieza que está arriba suyo
+  if(direccion == 40){
+    nuevaFilaPiezaVacia = posicionVacia.fila-1;
+    nuevaColumnaPiezaVacia = posicionVacia.columna;
+  }
+  // Intercambia pieza blanca con la pieza que está abajo suyo
+  else if (direccion == 38) {
+    nuevaFilaPiezaVacia = posicionVacia.fila+1;
+    nuevaColumnaPiezaVacia = posicionVacia.columna;
+
+  }
+  // Intercambia pieza blanca con la pieza que está a su izq
+  else if (direccion == 39) {
+    // Compl nueva
+    nuevaColumnaPiezaVacia = posicionVacia.columna-1;
+    nuevaFilaPiezaVacia = posicionVacia.fila;
+    
+
+  }
+  // Intercambia pieza blanca con la pieza que está a su der
+  else if (direccion == 37) {
+    // Completar}
+    nuevaColumnaPiezaVacia = posicionVacia.columna+1;
+    nuevaFilaPiezaVacia = posicionVacia.fila;
+   
+  }
+    console.log("columna2 "+nuevaColumnaPiezaVacia);
+  // Se chequea si la nueva posición es válida, si lo es, se intercambia 
+  if (posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia)){
+    intercambiarPosiciones(posicionVacia.fila, posicionVacia.columna, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+    actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+  }
+}
+
+// Extras, ya vienen dadas
+
+function mezclarPiezas(veces){
+  if(veces<=0){return;}
+  var direcciones = [40, 38, 39, 37];
+  var direccion = direcciones[Math.floor(Math.random()*direcciones.length)];
+  console.log("mover");
+  moverEnDireccion(direccion);
+
+  setTimeout(function(){
+    mezclarPiezas(veces-1);
+  },100);
+}
+
+function capturarTeclas(){
+  document.body.onkeydown = (function(evento) {
+      moverEnDireccion(evento.which);
+
+      var gano = chequearSiGano();
+      if(gano){
+        setTimeout(function(){
+          mostrarCartelGanador();  
+        },500);
+      } 
+      evento.preventDefault();
+    }) 
+}
+function iniciar(){
+  //debugger;
+  mezclarPiezas(60);
+  capturarTeclas();
+}
+
+
+iniciar();
